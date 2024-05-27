a501334741341222c80d2fd8779facbc4c6b4984
viral <viral@fosserp.com>
2024-05-27 04:44:47 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Demowarehouse
+
+Test
+
+#### License
+
+MIT
---
+++ demowarehouse/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Demowarehouse",
+			"type": "module",
+			"label": _("Demowarehouse")
+		}
+	]
